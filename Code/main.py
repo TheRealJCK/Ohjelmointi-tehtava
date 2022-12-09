c3f03c537c2128475822bcb96cf9298078617869
Jimmy Chen <AC2166@jamk.fi>
2022-12-09 14:17:21 +0200

    maybe ready?

--- Code/main.py
+++ Code/main.py
@@ -2,80 +2,95 @@ import os
 from os.path import join
 import json
 import xlrd 
+import pandas as pd
 
-file_name = input("Enter the name of the file (e.g. asiakas-data.xlsx): ")
-location = input("Enter the disc where the file is located (character only): ")
 
-def find_file(disc, look_for):
-    
-        print("Looking for the file...\nPlease wait...")
-        for root, dirs, files in os.walk(disc):
-                if look_for in files:
-                    path = os.path.join(root, look_for)
-                    print("File found!")
-                    return path
 
-location = location + ':\\'
-if os.path.exists(location) == True:
+test_path = r"C:\Users\Jimmy Chen\Downloads\anime (version 1).xlsb.xlsx"
+file_name = input("Enter the full file path (e.g. C:/Users\Jimmy Chen\Downloads\asiakas-data.xlsx):")
+#location = input("Enter the disc where the file is located (character only): ")
 
-    if  ".xlsx" in file_name:
-        look_for_file = file_name
-    else:
-        look_for_file = file_name + ".xlsx"
 
-    file_path = find_file(location, look_for_file)
+try:
+    df = pd.read_excel(file_name)
+except FileNotFoundError:
+    print("\n File not found! \n Please check the file path ")
+    exit()
 
-    try:
-        wb = xlrd.open_workbook(file_path)   
-    except TypeError:
-        print("\n File not found! \n Please check the file name and the disc location ")
-        exit()
+
+df.fillna('', inplace=True)
+result = df.to_dict(orient='records')
+
+print(result)
+print("\nConverting to JSON...")
+
+list_of_data = []
+end_result = {}
+Applications_In_Use_list = []
+adress_info = {}
+other_info = {}
+
+for i in range(len(result)):
+
+    for key, value in result[i].items():
+
+        # conver the value to string if it is not a string
+        if not type(value) == str:
+            value = int(value)
+            value = str(value)
+
+        # if the key is address, then make a dictionary of the address info
+        if "Address" in key:
+            key = key.rsplit('.', 1)[-1]
+            adress_info[key] = value
+
+        # if the key is ApplicationsInUse, then make a list of the applications in use
+        elif "ApplicationsInUse" in key:
+
+            if ";" in value:
+                for Applications in value.split(';'):
+                    Applications_In_Use_list.append(Applications)
+            else:
+                Applications_In_Use_list.append(value)
+            other_info[key] = Applications_In_Use_list
+            Applications_In_Use_list = []
         
-    ws = wb.sheet_by_index(0)
-
-    list_of_data = []
-    Applications_In_Use_list = []
-    end_result = {}
-    adress_info = {}
-    other_info = {}
-
-    for i in range(ws.nrows):
-
-        if i == 0:
-            pass
+        # if the key is not address or ApplicationsInUse, then add it to the other info dictionary
         else:
-            for  j in range(ws.ncols):
+            other_info[key] = value
 
-                var = ws.cell_value(i, j)
-                if not type(var) == str:
-                    var = int(var)
-                    var = str(var)
-                if "ApplicationsInUse" in ws.cell_value(0,j): 
-                    if ";" in var:
-                        for Applications in var.split(';'):
-                            Applications_In_Use_list.append(Applications)
-                    else:
-                        Applications_In_Use_list.append(var)
-                    other_info[ws.cell_value(0,j)] = Applications_In_Use_list
-                    Applications_In_Use_list = []
-                elif 'Address' in ws.cell_value(0,j):
-                    name = ws.cell_value(0,j).rsplit('.', 1)[-1]
-                    adress_info[name] = var
-                else:
-                    other_info[ws.cell_value(0,j)] = var
-                if j == ws.ncols-1:
+        if len(other_info) == 11:
+            end_result["Address"] = dict(adress_info) 
+            end_result.update(other_info)
+            list_of_data.append(end_result)
+            end_result = {}
+            other_info = {}
 
-                    end_result["Address"] = dict(adress_info)
-                    end_result.update(other_info)
+with open('output.json', 'w', encoding='utf-8') as f:
+    data_str = json.dumps(list_of_data, indent=4, ensure_ascii=False)
+    f.write(data_str)
 
-                    list_of_data.append(end_result)
-                    end_result = {}
-    with open('output.json', 'w', encoding='utf-8') as f:
-        data_str = json.dumps(list_of_data, indent=4, ensure_ascii=False)
-        f.write(data_str)
 
-    print("\nAll done!")
 
-else:
-    print("The disc does not exist \n Please try again")
+
+print("\nAll done!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
